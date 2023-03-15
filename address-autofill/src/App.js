a6a6339efb321a55f1b95c49a55e16505e493103
KobayashiYoh <y.kobayashi@4nonome.com>
2023-03-15 22:51:03 +0900

    テキストボックスを作成

--- address-autofill/src/App.js
+++ address-autofill/src/App.js
@@ -1,22 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [address, setAddress] = useState('');
+
+  const handleChangeAddress = (e) => {
+    setAddress(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    console.log(address);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          {address}
+          <input type="text" value={address} onChange={handleChangeAddress} />
+          <button onClick={handleSubmit} >Submit</button>
+        </div>
       </header>
     </div>
   );

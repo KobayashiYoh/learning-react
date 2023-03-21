8bd8fea8f2d9d6228581e28362ed365d01b5b68a
KobayashiYoh <y.kobayashi@4nonome.com>
2023-03-21 23:22:02 +0900

    猫の鳴き声ボイス再生機能を追加

--- play-sound/src/App.js
+++ play-sound/src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import sound from './cat.mp3';
+
+const handlePlay = (e) => {
+  console.log("play");
+  const audio = new Audio(sound);
+  audio.play();
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={handlePlay}>🐈</button>
       </header>
     </div>
   );

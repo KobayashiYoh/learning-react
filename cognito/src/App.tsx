3184fe25839be8dcb98e891b8a4b145aa0f2a24a
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-08-15 23:06:47 +0900

    cssを削除

--- cognito/src/App.tsx
+++ cognito/src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Amplify } from "aws-amplify";
+import "@aws-amplify/ui-react/styles.css";
+import { ConfirmationCodeInputPage } from "./pages/ConfirmationCodeInputPage";
 
-function App() {
-  const [count, setCount] = useState(0)
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: String(import.meta.env.VITE_AWS_USER_POOLS_ID), //メモっておいたユーザープールIDをここに!
+      userPoolClientId: String(import.meta.env.VITE_AWS_POOLS_WEB_CLIENT_ID), //メモっておいたクライアントIDをここに!
+      loginWith: {
+        oauth: {
+          domain: "XXXX.auth.ap-northeast-1.amazoncognito.com", //Cognitoドメイン(XXXX => 作成したドメイン名を挿入)
+          scopes: ["openid", "email", "profile"],
+          redirectSignIn: ["http://localhost:3000/login/home"], //ログイン後、リダイレクトするurl
+          redirectSignOut: ["http://localhost:3000/login"], //ログアウト後、リダイレクトするurl
+          responseType: "code",
+          providers: ["Google", { custom: "Line" }], //後述(ソーシャルログイン)
+        },
+      },
+    },
+  },
+});
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function App() {
+  return <ConfirmationCodeInputPage />;
 }
-
-export default App
